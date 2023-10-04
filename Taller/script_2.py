55e414b2e1c34631a4b29798a833738ce2fe412b
MyLo <kamandramsan@gmail.com>
2023-10-04 08:00:25 -0500

    oct-4

--- Taller/script_2.py
+++ Taller/script_2.py
@@ -2,46 +2,52 @@ import os
 import requests
 from colorama import Fore, Back, Style, init
 
+# OPERATOR_ROLE = {'estudiante': 3, 'profesor': 4, 'profesor_auxiliar': 5, 'diseñador': 6,
+#                  'observador': 7, 'master': 30, 'operador': 35, 'sim': 115}
+#
+
 init(autoreset=True)  # reset the previous color and style after each line
 ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
 api_url = f'https://poli.instructure.com/api/v1/'
 
 
-def settings_course(url='', payload={}, token=''):
+def matricular_usuario(url='', payload={}, token=''):
     headers = {
         "Authorization": f"Bearer {token}"
     }
-    # Recuperar las opciones del curos
-    r = requests.get(url, headers=headers, data=payload)
-    if r.status_code == 200:
-        response = r.json()
-        opcion = response["filter_speed_grader_by_student_group"]
-        if opcion:
-            print(Fore.RED + 'El filtro del SpeedGrader ya está modificado')
-            return r
-    r = requests.put(url, headers=headers, data=payload)
+    r = requests.post(url, headers=headers, data=payload)
     if r.status_code == 200:
         color = Fore.GREEN
     else:
         color = Fore.RED
     print(color + f'{r.status_code} >> {r.reason}')
-    return r
+    if r.status_code == 200:
+        return r
+    else:
+        return None
 
 
 string_list = input('Ingrese la lista de cursos separados por coma: ')
 course_list = string_list.split(",")
+user_id = input('Ingrese el id del usuario: ')
+rol_id = input('Ingrese el id del rol: ')
+
 token = ACCESS_TOKEN
 payload = {
-    "filter_speed_grader_by_student_group": True,
+    "enrollment[user_id]": user_id,
+    "enrollment[role_id]": rol_id,
+    "enrollment[enrollment_state]": "active"
 }
+
+
 n, m = 1, len(course_list)
 for course_id in course_list:
     print(Fore.BLUE + f'[{n}/{m}]: Ingresando a {course_id}')
-    url = f'{api_url}courses/{course_id}/settings'
-    r = settings_course(url, payload, token=ACCESS_TOKEN)
+    url = f'{api_url}/courses/{course_id}/enrollments'
+    r = matricular_usuario(url, payload, token=ACCESS_TOKEN)
     if r.status_code == 200:
         response = r.json()
-        print(f'\t filter_speed_grader: {response["filter_speed_grader_by_student_group"]}')
+        print(Fore.LIGHTGREEN_EX + f'\t El usuario {user_id} ha sido matriculado')
     else:
-        print(Fore.RED + '\t Error')
+        print('Error')
     n = + 1
