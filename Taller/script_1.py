d2cbfa6583ebaa36d39d6b9f754676ab7ac373bd
MyLo <kamandramsan@gmail.com>
2023-10-04 07:33:42 -0500

    oct-4

--- Taller/script_1.py
+++ Taller/script_1.py
@@ -10,6 +10,14 @@ def settings_course(url='', payload={}, token=''):
     headers = {
         "Authorization": f"Bearer {token}"
     }
+    # Recuperar las opciones del curos
+    r = requests.get(url, headers=headers, data=payload)
+    if r.status_code == 200:
+        response = r.json()
+        opcion = response["filter_speed_grader_by_student_group"]
+        if opcion:
+            print(Fore.RED + 'El filtro del SpeedGrader ya está modificado')
+            return r
     r = requests.put(url, headers=headers, data=payload)
     if r.status_code == 200:
         color = Fore.GREEN
@@ -21,15 +29,16 @@ def settings_course(url='', payload={}, token=''):
     else:
         return None
     
-course_list = [62739, 39217]
+string_list = input('Ingrese la lista de cursos separados por coma: ')
+course_list = string_list.split(",")
 token=ACCESS_TOKEN
 payload = {
     "filter_speed_grader_by_student_group": True,
 }
 n, m = 1, len(course_list)
 for course_id in course_list:
-    print(f'[{n}/{m}]: Ingresando a {course_id}')
-    url = f'{api_url}courses/{course_id}/settings' 
+    print(Fore.BLUE + f'[{n}/{m}]: Ingresando a {course_id}')
+    url = f'{api_url}courses/{course_id}/settings'
     r = settings_course(url, payload, token=ACCESS_TOKEN)
     if r.status_code == 200:
         response = r.json()
