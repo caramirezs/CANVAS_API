04a5fde5e7afd512045e7b9f0d9815582e7d0ea1
MyLo <kamandramsan@gmail.com>
2023-10-09 07:43:22 -0500

    oct-9

--- Taller/script_4.py
+++ Taller/script_4.py
@@ -1,6 +1,7 @@
 import os
 import requests
 import json
+import pandas as pd
 from colorama import Fore, Back, Style, init
 
 # OPERATOR_ROLE = {'estudiante': 3, 'profesor': 4, 'profesor_auxiliar': 5, 'diseñador': 6,
@@ -20,12 +21,25 @@ def prin_status_response(r):
     print(color + f'{r.status_code} >> {r.reason}')
 
 api_url = f'https://poli.instructure.com/api/v1/'
-url = f'{api_url}accounts/1/terms'
+url = f'{api_url}accounts/1/terms?per_page=100'
 headers = {
     "Authorization": f"Bearer {ACCESS_TOKEN}"
 }
 print("Recuperando la información de los periodos")
 r = requests.get(url=url, headers=headers)
+response = r.json()['enrollment_terms']
+links = r.links
 
-prin_status_response(r)
-print(r.json())
+n = 2
+while links.get('next') is not None:
+    url = r.links['next']['url']
+    r = requests.get(url, headers=headers)
+    response += r.json()['enrollment_terms']
+    links = r.links
+    n += 1
+print(f'Se exportaron {n-1} páginas\n'
+      f'total periodos: {len(response)}')
+#Crear DataFrame
+
+df = pd.DataFrame(response)
+df.to_excel(f'periodos.xlsx', index=False)
